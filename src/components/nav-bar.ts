76b5b8ee38990d9014fcfed2f379de500e55188c
Benjamin Pannell <admin@sierrasoftworks.com>
2016-06-28 13:52:03 +0200

    tweak: Rework navbar to work on mobile

--- src/components/nav-bar.ts
+++ src/components/nav-bar.ts
@@ -1,12 +1,17 @@
 import {autoinject, bindable} from "aurelia-framework";
+import {EventAggregator} from "aurelia-event-aggregator";
 import {Router} from "aurelia-router";
 import {Breadcrumbs} from "./breadcrumb";
 
 @autoinject
 export class NavBar {
-  constructor(private breadcrumbs: Breadcrumbs) {
-
+  constructor(private breadcrumbs: Breadcrumbs, private events: EventAggregator) {
+    events.subscribe("router:navigation:processing", () => {
+      this.sideNav = false;
+    });
   }
 
   @bindable router: Router;
+
+  sideNav: boolean = false;
 }
