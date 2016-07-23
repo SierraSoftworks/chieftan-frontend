8392419b5ed5c38503134eb92be3adc75a53f6c0
Benjamin Pannell <admin@sierrasoftworks.com>
2016-07-23 14:30:53 +0200

    feat: Add typings to repository

---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/highlightjs/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/moment-node/index.d.ts" />
+/// <reference path="modules/webpack/index.d.ts" />
