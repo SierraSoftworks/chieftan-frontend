ea23cdfb13b89ed1e37c199c6d04c5bffacbefa6
Benjamin Pannell <admin@sierrasoftworks.com>
2016-09-08 13:09:44 +0200

    feat: Allow removing environments

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import {APIBase} from "./api/common";
 import {StatusAPI} from "./api/status";
 import {UsersAPI} from "./api/users";
 import {UserManager} from "./managers/user";
-import {EnvironmentManager} from "./managers/environments";
+import {EnvironmentManager, Environment} from "./managers/environments";
 import {CodeSnippetConfig, CodeSnippetLanguage, CodeSnippetGenerators} from "./components/code-snippets";
 
 @autoinject
@@ -45,6 +45,18 @@ export class ConfigView {
 
     this.envs.environments.push(env);
     this.envs.active = env;
+  }
+
+  removeEnvironment(env: Environment) {
+    const i = this.envs.environments.indexOf(env);
+    if (~i) this.envs.environments.splice(i, 1);
+
+    if (this.envs.active === env) {
+      if (!this.envs.environments.length) return this.newEnvironment();
+      else this.envs.active = this.envs.environments[0];
+    }
+    
+    this.envs.save();
   }
 
   saveEnvironments() {
